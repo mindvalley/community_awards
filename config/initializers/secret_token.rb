e6b781d1e88456206807ae219bd945dd22f18a64
Muhammad Furqan Asghar <furqan@mindvalley.com>
2013-05-07 11:29:28 +0800

    initial project directory structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwardsV2::Application.config.secret_token = '20b265fb6f18729632f7ebab5a73a4f93ce300cc4933f82ab4c3c147ab84c6e3ce0b386d87992fc973d6a8468ef727308f4592dbb45ab2c21d5009788625a4a5'
