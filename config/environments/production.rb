754d6e5985a4414ecff12fba51aa2ccc923430ec
Muhammad Furqan Asghar <furqan@mindvalley.com>
2013-06-24 19:19:18 +0800

    fixed missing comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ AwardsV2::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE
+    :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
     :address => 'awards.mindvalley.net',
     :port => 25,
     :domain => 'awards.mindvalley.net'
